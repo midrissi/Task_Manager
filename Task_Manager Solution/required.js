bb0225d1686cc64289359fb47a1bb83d0f64b311
IDRISSI Mohamed <mohamed.idrissi@4d.com>
2013-08-30 22:58:10 +0100

    Update required.js

--- Task_Manager Solution/required.js
+++ Task_Manager Solution/required.js
@@ -1,1 +1,25 @@
-function loginHandler(userName, passwordOrKey, secondIsAKey) {    if (application.name === 'ToDo') {        var user = ds.User.find('login = :1 or email = :1', userName);        if (user !== null) {            if (user.passwordIsValid(passwordOrKey, secondIsAKey)) {                return {                    name: userName,                    fullName: user.fullname,                    belongsTo: user.group?user.group.split(','): [],                    storage: {                        ID: user.getKey()                    }                };            } else {                return {                    error: 121,                    errorMessage: 'Password is Invalid'                };            }        }    }    return false;}
+function loginHandler(userName, passwordOrKey, secondIsAKey) {
+    if (application.name === 'Task_Manager') {
+        var user = ds.User.find('login = :1 or email = :1', userName);
+
+        if (user !== null) {
+            if (user.passwordIsValid(passwordOrKey, secondIsAKey)) {
+                return {
+                    name: userName,
+                    fullName: user.fullname,
+                    belongsTo: user.group?user.group.split(','): [],
+                    storage: {
+                        ID: user.getKey()
+                    }
+                };
+            } else {
+                return {
+                    error: 121,
+                    errorMessage: 'Password is Invalid'
+                };
+            }
+        }
+    }
+
+    return false;
+}
